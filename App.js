7c57ea1aa0b79dd4d5892b588a15cd3a9f405e40
Weizhe Gao <52844604+Quixot1k@users.noreply.github.com>
2023-07-19 19:12:30 -0700

    make notification request

--- App.js
+++ App.js
@@ -3,27 +3,37 @@ import StackNavigator from "./src/navigation/StackNavigator";
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {useEffect} from "react";
 import messaging from '@react-native-firebase/messaging';
-import {Alert} from "react-native";
+import {Alert, PermissionsAndroid, Platform} from 'react-native';
 
 const queryClient = new QueryClient();
+
 export default function App() {
   useEffect(() => {
     async function requestUserPermission() {
-      const authStatus = await messaging().requestPermission();
-      const enabled =
-        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-      if (enabled) {
-        console.log('Authorization status:', authStatus && "enabled");
+      if (Platform.OS === 'ios') {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+          authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+          authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+        if (enabled) {
+          console.log('Authorization status:', authStatus && "enabled");
+        }
+      } else if (Platform.OS === 'android') {
+        await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
       }
+
     }
 
     // Request permission
-    requestUserPermission().catch(err => console.log(err));
+    requestUserPermission().catch(err => {
+      console.log(err)
+    });
+
     // Register foreground handler
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
     });
+
     // Register background handler
     messaging().setBackgroundMessageHandler(async remoteMessage => {
       console.log('Message handled in the background!', remoteMessage);
