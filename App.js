6bc0c0b2371a778ae79bbed2ac5471f1a276d274
Weizhe Gao <52844604+Quixot1k@users.noreply.github.com>
2023-07-22 21:39:46 -0700

    first test on expo-notification

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import {NavigationContainer} from "@react-navigation/native";
 import StackNavigator from "./src/navigation/StackNavigator";
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import {Platform} from "react-native";
+import {useEffect, useRef} from "react";
 // import {useEffect} from "react";
 // import messaging from '@react-native-firebase/messaging';
 // import {Alert, PermissionsAndroid, Platform} from 'react-native';
 
 const queryClient = new QueryClient();
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const {status: existingStatus} = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const {status} = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
 
 export default function App() {
   // useEffect(() => {
@@ -40,6 +83,24 @@ export default function App() {
   //   });
   //   return unsubscribe;
   // }, []);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => console.log(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
