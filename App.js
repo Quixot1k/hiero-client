1404ba7b931bb78910a3c1e2270418109208b922
Weizhe Gao <52844604+Quixot1k@users.noreply.github.com>
2023-07-25 19:47:57 -0700

    updated navigation packages

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import * as Notifications from 'expo-notifications';
 import {useEffect, useRef} from "react";
 import registerForPushNotificationsAsync from "./src/utils/registerForPushNotificationsAsync";
+// import * as TaskManager from "expo-task-manager";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,17 @@ Notifications.setNotificationHandler({
     shouldSetBadge: true,
   }),
 });
+
+// background
+// const BACKGROUND_NOTIFICATION_TASK = 'BACKGROUND-NOTIFICATION-TASK';
+// TaskManager.defineTask(BACKGROUND_NOTIFICATION_TASK, ({data, error, executionInfo}) => {
+//   console.log('Received a notification in the background!');
+//   // Do something with the notification data
+//   console.log(data)
+// });
+// Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK).then(err => {
+//   console.log(err)
+// });
 
 export default function App() {
   const navigationRef = useRef(null);
@@ -28,9 +40,9 @@ export default function App() {
     });
 
     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
       navigationRef.current?.navigate("ScheduleScreen_Notification")
     });
+
     return () => {
       Notifications.removeNotificationSubscription(notificationListener.current);
       Notifications.removeNotificationSubscription(responseListener.current);
